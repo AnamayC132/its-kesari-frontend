460e282b291d12323fa3f4ac80800dabb1b77c52
Anamay Pandey <anamaypandey1@gmail.com>
2020-12-18 13:00:53 +0530

    Trigger Heroku deploy after enabling collectstatic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MoviesService } from './movies/movies.service';
-
+import {MatCardModule} from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,6 +21,9 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
   ],
   providers: [MoviesService],
   bootstrap: [AppComponent]
